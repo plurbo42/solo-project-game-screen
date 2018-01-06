8d5b57bf4f98301df4d6dc1100c5114b8dbb7da4
Paige Scheunemann <pls927@gmail.com>
2018-01-05 22:51:36 -0600

    encounter initative roll

--- server/public/scripts/controllers/encounter.controller.js
+++ server/public/scripts/controllers/encounter.controller.js
@@ -1,4 +1,4 @@
-app.controller('EncounterController', function(UserService, EncounterService) {
+app.controller('EncounterController', function(UserService, EncounterService, $scope) {
     console.log('EncounterController created');
     var self = this;
     self.userService = UserService;
@@ -6,6 +6,22 @@ app.controller('EncounterController', function(UserService, EncounterService) {
     EncounterService.getEncounter();
     self.getCurrentEncounter = EncounterService.currentEncounter;
     self.currentEncounterArray = EncounterService.currentEncounterArray;
+
+    //this does not work yet
+    $scope.$on('dragToReorder.dropped', (e, data) => {
+      self.encounters = data.list.map((encounter, i) => {
+        encounter.rank = i + 1;
+        return encounter;
+      });
+    });
+
+    self.rollInitiative = function(characterArray) {
+      for (let i = 0; i < characterArray.length; i++) {
+        var character = characterArray[i];
+        character.initiative = (Math.floor(Math.random() * 20)) + 1;
+        console.log(character.initiative);
+      }
+    }
 
   });
   
